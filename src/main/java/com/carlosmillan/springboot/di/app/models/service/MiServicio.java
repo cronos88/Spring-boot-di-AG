88bda1f0e608d0c83afb5366767a287a84ef39a0
cronos88 <atlantis9@hotmail.com>
2023-10-28 22:40:49 -0500

    Se crea el servicio MiServicio y el template index, antes de DI

---
+++ src/main/java/com/carlosmillan/springboot/di/app/models/service/MiServicio.java
@@ -0,0 +1,7 @@
+package com.carlosmillan.springboot.di.app.models.service;
+
+public class MiServicio {
+    public String operacion() {
+        return "ejecutando algun proceso importante...";
+    }
+}
