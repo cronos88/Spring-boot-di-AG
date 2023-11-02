6e62ed388f7b360469fad49ec101d9163f79817b
cronos88 <atlantis9@hotmail.com>
2023-11-01 21:26:40 -0500

    Usando la anotación @Primary para indicar cual clase que implemente IServicio usar como primaria

--- src/main/java/com/carlosmillan/springboot/di/app/models/service/MiServicio.java
+++ src/main/java/com/carlosmillan/springboot/di/app/models/service/MiServicio.java
@@ -1,10 +1,12 @@
 package com.carlosmillan.springboot.di.app.models.service;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component("MiServicioSimple")
+@Primary
 public class MiServicio implements IServicio{
     public String operacion() {
-        return "ejecutando algun proceso importantisimo..";
+        return "ejecutando algun proceso importante simple ..";
     }
 }
