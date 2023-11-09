a4f7e276219f882b39bc129e76c6db27326c9d5b
cronos88 <atlantis9@hotmail.com>
2023-11-08 22:33:06 -0500

    Registrando el componente ItemsFactura

--- src/main/java/com/carlosmillan/springboot/di/app/models/domain/ItemFactura.java
+++ src/main/java/com/carlosmillan/springboot/di/app/models/domain/ItemFactura.java
@@ -2,22 +2,27 @@ package com.carlosmillan.springboot.di.app.models.domain;
 
 public class ItemFactura {
 
-    private Producto producto;
-    private Integer cantidad;
+	private Producto producto;
+	private Integer cantidad;
 
-    public Producto getProducto() {
-        return producto;
-    }
+	public ItemFactura(Producto producto, Integer cantidad) {
+		this.producto = producto;
+		this.cantidad = cantidad;
+	}
 
-    public void setProducto(Producto producto) {
-        this.producto = producto;
-    }
+	public Producto getProducto() {
+		return producto;
+	}
 
-    public Integer getCantidad() {
-        return cantidad;
-    }
+	public void setProducto(Producto producto) {
+		this.producto = producto;
+	}
 
-    public void setCantidad(Integer cantidad) {
-        this.cantidad = cantidad;
-    }
+	public Integer getCantidad() {
+		return cantidad;
+	}
+
+	public void setCantidad(Integer cantidad) {
+		this.cantidad = cantidad;
+	}
 }
